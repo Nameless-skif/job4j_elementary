83a4a150288520803fe108d0f0f1d61b20f88cb3
Nameless-skif <123570283+Nameless-skif@users.noreply.github.com>
2023-05-20 14:11:20 +0300

    5. Конвертер валюты [#41583]

--- src/main/java/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -13,8 +13,8 @@ public class Converter {
 
     public static void main(String[] args) {
         float euro = Converter.rubleToEuro(140);
-        float dollar = Converter.rubleToDollar(140);
+        float dollars = Converter.rubleToDollar(140);
         System.out.println("140 rubles are " + euro + " euro.");
-        System.out.println("140 rubles are " + dollar + " dollar.");
+        System.out.println("140 rubles are " + dollars + " dollar.");
     }
 }
