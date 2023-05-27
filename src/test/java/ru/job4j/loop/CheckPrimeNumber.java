e5d594bbf5847ebfb8cce3675a0c1fe2b4b8f3d8
Nameless-skif <123570283+Nameless-skif@users.noreply.github.com>
2023-05-27 13:40:19 +0300

    5.5. Простое число (оператор прерывания break) [#171694]

---
+++ src/test/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -0,0 +1,12 @@
+package ru.job4j.loop;
+
+public class CheckPrimeNumber {
+    @SuppressWarnings("checkstyle:WhitespaceAfter")
+    public static boolean check(int number) {
+        boolean prime = false;
+        if (number % 2 != 0 && number > 1 && number % number == 0) {
+            prime = true;
+        }
+        return prime;
+    }
+}
