8cbe714b597067e96f589f3dcd1c4599ad9599e7
Nameless-skif <123570283+Nameless-skif@users.noreply.github.com>
2023-05-31 07:56:44 +0300

    6.6.0. Двумерный массив [#226796 #419797]

--- src/main/java/ru/job4j/array/Sequence.java
+++ src/main/java/ru/job4j/array/Sequence.java
@@ -9,8 +9,8 @@ public class Sequence {
         array[1][0] = 4;
         array[1][1] = 5;
         array[1][2] = 6;
-        array[3][0] = 7;
-        array[3][1] = 8;
-        array[3][2] = 9;
+        array[2][0] = 7;
+        array[2][1] = 8;
+        array[2][2] = 9;
     }
 }
