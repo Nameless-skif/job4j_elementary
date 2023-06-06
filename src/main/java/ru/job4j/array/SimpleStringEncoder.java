52b5a64b2f563c5794321c6f4dc7cd28e20b7baa
Nameless-skif <123570283+Nameless-skif@users.noreply.github.com>
2023-06-06 18:57:11 +0300

    3. Тестовое задание. Блок "Базовый синтаксис" [#505049 #420765]

--- src/main/java/ru/job4j/array/SimpleStringEncoder.java
+++ src/main/java/ru/job4j/array/SimpleStringEncoder.java
@@ -5,20 +5,16 @@ public class SimpleStringEncoder {
         String result = "";
         char symbol = input.charAt(0);
         int counter = 1;
-        for (int i = 1; i < input.length(); i++) {
-            if (symbol != input.charAt(i)) {
-                result += counter > 1 ? input.charAt(i - 1) + "" +  counter : input.charAt(i - 1);
-                counter = 1;
-                symbol = input.charAt(i);
-                result += i == input.length() - 1 ? input.charAt(i) : "";
-                result += (i == input.length() - 1) && (counter > 1) ? counter : "";
-            } else {
+        for (int i = 0; i < input.length() - 1; i++) {
+            if (symbol == input.charAt(i + 1)) {
                 counter++;
-                result += i == input.length() - 1 ? input.charAt(i) : "";
-                result += (i == input.length() - 1) && (counter > 1) ? counter : "";
+            } else {
+                result += counter > 1 ? symbol + "" +  counter : symbol;
+                counter = 1;
+                symbol = input.charAt(i + 1);
             }
             }
-
+        result += counter > 1 ? symbol + "" +  counter : symbol;
         return result;
     }
 }
